e49c49ce24eeb0ff9720a770f2191590778d784c
Andrei <buha2595@gmail.com>
2025-12-23 15:54:11 +0100

    boards model created v2

--- boards_app/models.py
+++ boards_app/models.py
@@ -5,30 +5,29 @@ from django.contrib.auth.models import User
 class Board(models.Model):
     # "title": "Projekt Y",
     title = models.CharField(max_length=100)
+    # "owner_id": 3
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     members = models.ManyToManyField(User, blank=True, related_name='member_boards')
-
-    # "member_count": 12,
-    # "ticket_count": 43,
-    #     "tasks_to_do_count": 12,
-    # "tasks_high_prio_count": 1,
-    # "owner_id": 3
+    
     def __str__(self):
         return self.title
     
-    # Contorizări dinamice
     @property
+    # "member_count": 12,
     def member_count(self):
         return self.members.count()
 
     @property
+    # "ticket_count": 43,
     def ticket_count(self):
-        return self.tasks.count()  # tasks = related_name din Task
+        return self.tasks.count()
 
     @property
+    #"tasks_to_do_count": 12,
     def tasks_to_do_count(self):
         return self.tasks.filter(status='TODO').count()
 
     @property
+    # "tasks_high_prio_count": 1,
     def tasks_high_prio_count(self):
         return self.tasks.filter(priority='HIGH').count()
