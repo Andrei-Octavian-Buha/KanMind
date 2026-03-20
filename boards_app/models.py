b74de2226b35015d21b9c21b4ce441a15bf365a0
Andy <buha2595@gmail.com>
2026-03-20 22:41:24 +0100

    board_app added

--- boards_app/models.py
+++ boards_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BoardsModel(models.Model):
+    #"title": "neu"
+    title = models.CharField(max_length=100)
+    #"owner_id": 2
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    #"members": []
+    members = models.ManyToManyField(User, blank=True, related_name='member_boards')
+
+    def __str__(self):
+        return self.title
